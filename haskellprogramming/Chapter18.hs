e4892ca5707bdad50a79d8a40e1da647716b9b58
Moony <moony.chen@perficient.com>
2018-04-20 10:05:45 +0800

    finish ch18

--- haskellprogramming/Chapter18.hs
+++ haskellprogramming/Chapter18.hs
@@ -200,6 +200,25 @@ j m = m >>= id
 --   m <- m'
 --   return m
 
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f ma mb = f <$> ma <*> mb
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a ma mf = mf <*> ma
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = return []
+meh (x:xs) f = do  -- return <$> f x .... (meh xs f)
+  b <- f x
+  bs <- meh xs f
+  return (b:bs)
+
+flipType :: (Monad m) => [m a] -> m [a]
+flipType as = meh as id
+
 main :: IO ()
 main = do
   quickBatch (functor (undefined :: Nope (Int, String, String)))
