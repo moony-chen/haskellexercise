39e6c527f336671a1fe752fa862f30aa8427d355
Moony Chen <moony.chen@perficient.com>
2018-05-24 00:08:31 +0800

    add ch20

---
+++ haskellprogramming/Chapter20.hs
@@ -0,0 +1,25 @@
+module Chapter20 where
+
+import Data.Monoid
+import Data.Foldable
+
+data Identity a = Identity a
+
+instance Foldable Identity where
+  foldr f z (Identity x) = f x z
+  foldl f z (Identity x) = f z x
+  foldMap f (Identity x) = f x
+
+-- -------------------------------------
+data Optional a =
+    Nada
+    | Yep a
+    deriving (Eq, Show)
+
+instance Foldable Optional where
+  foldr _ z Nada = z
+  foldr f z (Yep x) = f x z
+  foldl _ z Nada = z
+  foldl f z (Yep x) = f z x
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
