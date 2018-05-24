e7e14a513beabe351eaf8355c1c0f9f7812589c3
Moony Chen <moony.chen@perficient.com>
2018-05-24 22:40:17 +0800

    update ch20

--- haskellprogramming/Chapter20.hs
+++ haskellprogramming/Chapter20.hs
@@ -23,3 +23,25 @@ instance Foldable Optional where
   foldl f z (Yep x) = f z x
   foldMap _ Nada = mempty
   foldMap f (Yep a) = f a
+
+sum' :: (Foldable t, Num a) => t a -> a
+sum' = foldr (+) 0
+
+sum'' :: (Foldable t, Num a) => t a -> a
+sum'' xs = getSum $ foldMap Sum xs
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' e xs = getAny $ foldMap (Any . ( (==) e)) xs
+
+newtype Min a = Min {getMin :: Maybe a} deriving (Eq, Show)
+
+instance Ord a => Monoid (Min a) where
+  mempty = Min Nothing
+  mappend (Min Nothing) (Min Nothing) = Min Nothing
+  mappend (Min Nothing) (Min (Just y)) = Min $ Just y
+  mappend (Min (Just x)) (Min Nothing) = Min $ Just x
+  mappend (Min (Just x)) (Min (Just y)) = Min $ Just $ min x y
+
+
+minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+minimum' xs = getMin $ foldMap (Min . Just) xs
