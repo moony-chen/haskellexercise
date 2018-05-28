6d46503e35d2182b5c7d836f33f34baa6a236e2e
Moony <moony.chen@perficient.com>
2018-05-28 17:34:58 +0800

    update ex

--- haskellprogramming/Chapter21.hs
+++ haskellprogramming/Chapter21.hs
@@ -74,8 +74,70 @@ instance Eq a => EqProp (Constant a b) where
 instance Traversable (Constant a) where
   traverse _ (Constant a) = pure $ Constant a
 
+--------------------------
+
+data Optional a = Nada | Yep a deriving (Eq, Ord, Show)
+
+instance Functor Optional where
+  fmap _ Nada = Nada
+  fmap f (Yep a) = Yep $ f a
+
+instance Foldable Optional where
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
+
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Yep a
+
+instance Eq a => EqProp (Optional a) where
+  a =-= b = eq a b
+
+instance Traversable Optional where
+  traverse _ Nada = pure Nada
+  traverse f (Yep a) = Yep <$> f a
+
+--------------
+
+data List a = Nil | Cons a (List a) deriving (Eq, Ord, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a as) = Cons (f a) (fmap f as)
+
+instance Foldable List where
+  foldMap _ Nil = mempty
+  foldMap f (Cons a as) = f a <> foldMap f as
+
+take' :: Int -> List a -> List a
+take' 0 _ = Nil
+take' _ Nil = Nil
+take' n (Cons a as) = Cons a (take' (n - 1) as)
+
+-- http://codingstruggles.com/haskell/arbitrary-length-lists-quickcheck.html
+arbitraryList :: Arbitrary a => Int -> Gen (List a)
+arbitraryList m
+    | m == 0 = return Nil
+    | m > 6 = arbitraryList 6
+    | otherwise = Cons <$> arbitrary <*> arbitraryList (m-1)
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = sized arbitraryList
+
+instance Eq a => EqProp (List a) where
+  xs =-= ys = xs' `eq` ys'
+    where xs' = take' 3000 xs
+          ys' = take' 3000 ys
+
+instance Traversable List where
+  traverse _ Nil = pure Nil
+  traverse f (Cons a as) = liftA2 Cons (f a) (traverse f as)
+
+
+
 main = do
-let triggerIdentity = undefined :: Identity (String, Int, [Int])
-let triggerConstant = undefined :: Constant Int (String, Int, [Int])
-quickBatch (traversable triggerIdentity)
-quickBatch (traversable triggerConstant)
+quickBatch (traversable (undefined :: Identity (String, Int, [Int])))
+quickBatch (traversable (undefined :: Constant Int (String, Int, [Int])))
+quickBatch (traversable (undefined :: Optional (String, Int, [Int])))
+quickBatch (traversable (undefined :: List (Int, String, String)))
